df4f7955d650b7ed6c2673711c57f2278322d230
karur4n <karuran.f@gmail.com>
2020-06-06 16:19:06 +0900

    Add

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-const isProduction = process.env.NODE_ENV === 'production'
-
 module.exports = {
-  purge: isProduction ? ['src/**/*.html', 'src/**/*.ts', 'src/**/*.tsx'] : [],
+  purge: ['src/**/*.html', 'src/**/*.ts', 'src/**/*.tsx'],
   theme: {
     extend: {},
   },
